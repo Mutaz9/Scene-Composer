c0f09adb6ffab07b371ceeb5218cce18cd23dda7
Mutaz9 <105200830+Mutaz9@users.noreply.github.com>
2022-08-08 20:42:35 -0400

    added to folder

---
+++ scenes/Bubbles.java
@@ -0,0 +1,29 @@
+package assignment9.scenes;
+
+import assignment9.Drawable;
+
+public class Bubbles implements Drawable {
+	
+	private Circle[] bubbles;
+	
+	@Override
+	public String toString() {
+		return "Bubbles";
+	}
+	
+	public Bubbles(int num) {
+		this.bubbles = new Circle[num];
+		for (int i=0; i < num; ++i) {
+			bubbles[i] = new Circle(Math.random(), Math.random(), Math.random()/20);
+		}
+	}
+
+	@Override
+	public void draw() {
+		for (Circle c : bubbles) {
+			c.draw();
+		}
+		
+	}
+
+}
